e0975197fa5e1b382e4288cdb3ff11a915514405
thanhloc120299 <nguyenloc12021999@gmail.com>
2020-10-31 19:47:19 +0700

    asa

--- student/config.js
+++ student/config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    aws_table_name: 'student',
+    aws_table_name: 'sinhvien',
     aws_local_config: {
         region: 'local',
         endpoint: 'http://localhost:3000'
       //Provide details for local configuration
     },
     aws_remote_config: {
-      accessKeyId: 'AKIAS6PR2AJMDS64J26A',
-      secretAccessKey: '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',
+      accessKeyId: 'AKIAS6PR2AJMBSZPS7B3',
+      secretAccessKey: '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',
       region: 'us-east-2',}
 
 }
